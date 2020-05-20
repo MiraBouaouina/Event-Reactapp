dcea59756d009c6559be4721e8e61bf42274fb43
Amira Bouaouina <bouaouinamira@gmail.com>
2020-05-20 01:32:28 +0100

    first commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import "assets/css/bootstrap.min.css";
+import "assets/scss/now-ui-kit.scss";
+import "assets/demo/demo.css";
+import "assets/demo/nucleo-icons-page-styles.css";
 import 'bootstrap/dist/css/bootstrap.css';
 
 
